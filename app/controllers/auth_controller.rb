e40a86e2f473f85a7ded8aa0445bebfc6fb01d65
Andrey Kuznetsov <tormozz48@gmail.com>
2012-08-21 23:16:28 +0300

    fix for heroku deploy

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,27 +1,23 @@
-class AuthController::OmniauthCallbacksControllerController < Devise::OmniauthCallbacksController
+class AuthController < Devise::OmniauthCallbacksController
 
-#callback for facebook authentification
   def facebook
     @user = User.find_or_create_for_facebook env["omniauth.auth"]
     flash[:notice] = "Facebook"
     sign_in_and_redirect @user, :event => :authentication
   end
 
-  #callback for twitter authentification
   def twitter
     @user = User.find_or_create_for_twitter env["omniauth.auth"]
     flash[:notice] = "Twitter"
     sign_in_and_redirect @user, :event => :authentication
   end
 
-  #callback for vkontakte authentification
   def vkontakte
     @user = User.find_or_create_for_vkontakte env["omniauth.auth"]
     flash[:notice] = "Vkontakte"
     sign_in_and_redirect @user, :event => :authentication
   end
 
-  #callback for google authentification
   def google
     @user = User.find_or_create_for_google env["omniauth.auth"]
     flash[:notice] = "Google"
